3a2e4c6df8dd7e71ba6ec6c5956833fb6b34c053
Francesco Bianco <info.francescobianco@gmail.com>
2021-10-08 01:27:01 +0200

    First commit

---
+++ src/Bot.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$channelSecret = getenv('LINE_BOT_CHANNEL_SECRET');
+$channelAccessToken = getenv('LINE_BOT_CHANNEL_ACCESS_TOKEN');
+$userId = getenv('LINE_BOT_USER_ID');
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello 2');
+
+$response = $bot->pushMessage($userId, $textMessageBuilder);
+
+var_dump($response);
